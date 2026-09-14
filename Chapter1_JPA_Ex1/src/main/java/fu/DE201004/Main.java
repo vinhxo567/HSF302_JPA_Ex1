3831bb43d1623cc39fa8d228d9db55b5063a820c
Vinh <letrunghieu197401@gmail.com>
2026-09-14 14:01:44 +0700

    TODO 0.8

--- Chapter1_JPA_Ex1/src/main/java/fu/DE201004/Main.java
+++ Chapter1_JPA_Ex1/src/main/java/fu/DE201004/Main.java
@@ -1,11 +1,44 @@
 package fu.DE201004;
+import fu.DE201004.dao.EmployeeDAO;
+import fu.DE201004.pojo.Employee;
+import fu.DE201004.pojo.Gender;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsf302FU");
-        System.out.println("EMF tao thanh cong!");
+        EmployeeDAO dao = new EmployeeDAO();
+
+
+        Employee emp = new Employee("Nguyen Van A", "a@fpt.edu.vn",
+                new BigDecimal("15000000"), Gender.MALE, LocalDate.of(2022, 3, 1));
+
+        dao.save(emp);
+
+        System.out.println("Da tao: " + emp);
+
+
+        Employee found = dao.findById(emp.getId());
+
+        System.out.println("Doc lai: " + found);
+
+
+        found.setSalary(new BigDecimal("17000000"));
+
+        Employee updated = dao.update(found);
+
+        System.out.println("Sau update: " + updated);
+
+
+        Employee reChecked = dao.findById(emp.getId());
+        System.out.println("Kiem tra lai sau update: " + reChecked);
+
+
+        dao.delete(emp.getId());
+
+        Employee afterDelete = dao.findById(emp.getId());
+        System.out.println("Sau khi xoa, tim lai: " + afterDelete); // ky vong: null
     }
 }
+
