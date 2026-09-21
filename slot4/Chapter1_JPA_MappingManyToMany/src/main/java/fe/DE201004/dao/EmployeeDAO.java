4f839091910d8288804c2d9c5ea642c0756b4449
Vinh <letrunghieu197401@gmail.com>
2026-09-21 23:53:22 +0700

    feat(TODO 5.6): add assignEmployeeToProject in EmployeeDAO

--- slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fe/DE201004/dao/EmployeeDAO.java
+++ slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fe/DE201004/dao/EmployeeDAO.java
@@ -1,6 +1,7 @@
 package fe.DE201004.dao;
 
 import fe.DE201004.pojo.Employee;
+import fe.DE201004.pojo.Project;
 import fe.DE201004.util.JPAUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
@@ -68,6 +69,29 @@ public class EmployeeDAO {
             Employee employee = em.find(Employee.class, id);
             if (employee != null) {
                 em.remove(employee);
+            }
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx.isActive()) tx.rollback();
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    // TODO 5.6: Viết method assignEmployeeToProject(Long employeeId, Long projectId)
+    public void assignEmployeeToProject(Long employeeId, Long projectId) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            // find cả 2 entity trong 1 transaction
+            Employee employee = em.find(Employee.class, employeeId);
+            Project project = em.find(Project.class, projectId);
+            
+            if (employee != null && project != null) {
+                // gọi helper method để đồng bộ hóa 2 chiều
+                employee.assignToProject(project);
             }
             tx.commit();
         } catch (Exception ex) {
